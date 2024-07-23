650f3d760206c6843039ed3991296b933034b395
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-23 17:50:09 -0400

    updated styling for contact and portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,15 +2,19 @@ export default function Portfolio() {
     return (
         <div>
         <h1>Portfolio</h1>
+        <br></br>
         <a id="" className="" href="">
         👽 - Example 1 
         </a>
+        <br></br>
         <a id="" className="" href="">
         🛸 - Example 2 
         </a>
+        <br></br>
         <a id="" className="" href="">
         💀 - Example 3 
         </a>
+        <br></br>
         <a id="" className="" href="">
         🥑 - Example 4 
         </a>
