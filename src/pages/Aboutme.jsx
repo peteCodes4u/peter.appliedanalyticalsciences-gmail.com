c8b73b3ff7a9175921e2abb0e8d1dbc3078fdf0b
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-23 16:40:06 -0400

    configuration of pages complete for setup

---
+++ src/pages/Aboutme.jsx
@@ -0,0 +1,11 @@
+export default function Aboutme() {
+    return (
+        <div>
+        <h1>About Me</h1>
+        <p>
+            I am Peter Smith, a developer from Tampa Florida. Professionally I am a MERN full stack developer with many skills. I have experience with languages such as C# and JavaScript, and have developed automation test framworks for API's. I also have expereience with front end web application automation. 
+            My interests and passions include engineering eloquent solutions for any challenge. I am great with end to end analysis, evaluation, reverse-engineering and development. 
+        </p>
+      </div>
+    );
+};
