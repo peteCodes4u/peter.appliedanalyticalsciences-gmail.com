196a07b86980e997ad434dbe72da220cc7bcb0c0
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-25 17:31:01 -0400

    changes to portfolio - 1

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,23 +1,77 @@
+import grfxDxnrGif from '../assets/grafxDznr_p_gif.gif';
+
 export default function Portfolio() {
-    return (
-        <div>
-        <h2>Portfolio</h2>
-        <br></br>
-        <a id="" className="" href="">
-        👽 - Example 1 
-        </a>
-        <br></br>
-        <a id="" className="" href="">
-        🛸 - Example 2 
-        </a>
-        <br></br>
-        <a id="" className="" href="">
-        💀 - Example 3 
-        </a>
-        <br></br>
-        <a id="" className="" href="">
-        🥑 - Example 4 
-        </a>
-      </div>
-    )
+  return (
+
+    <div id="portfolio-bucket">
+    <h2 className="page-title">Portfolio</h2>
+    <br></br>
+    <section className="portfolio-topics">
+    <a href="https://github.com/peteCodes4u/grfxDznr">
+    <img id="" className="portfolio-item" src={grfxDxnrGif}></img>
+    <h4>👽 grfxDznr - node.js svg generator</h4>
+    </a>
+    <a href="https://github.com/peteCodes4u/worXpace">
+    <img id="" className="portfolio-item" src={grfxDxnrGif}></img>
+    <h4>🛸 worXpace - node.js and postgres data managment framework</h4>
+    </a>
+    <a href="https://github.com/peteCodes4u/grfxDznr">
+    <img id="" className="portfolio-item" src={grfxDxnrGif}></img>
+    <h4>👽 grfxDznr - node.js svg generator</h4>
+    </a>
+    </section>
+    <section className="portfolio-topics">
+    <a href="https://github.com/peteCodes4u/grfxDznr">
+    <img id="" className="portfolio-item" src={grfxDxnrGif}></img>
+    <h4>👽 grfxDznr - node.js svg generator</h4>
+    </a>
+    <a href="https://github.com/peteCodes4u/grfxDznr">
+    <img id="" className="portfolio-item" src={grfxDxnrGif}></img>
+    <h4>👽 grfxDznr - node.js svg generator</h4>
+    </a>
+    <a href="https://github.com/peteCodes4u/grfxDznr">
+    <img id="" className="portfolio-item" src={grfxDxnrGif}></img>
+    <h4>👽 grfxDznr - node.js svg generator</h4>
+    </a>
+    </section>
+    </div>
+    
+    // <section id="portfolio-bucket">
+    //   <section id="page-title">
+    //   <h2>Portfolio</h2>
+    //   </section> 
+    //   <section className="portfilio-cards">
+    //     <a id="" className="" href="">
+    //       <section className="card">
+    //         <h3>👽 - Example 1 </h3>
+    //       </section>
+    //     </a>
+    //     <a id="" className="" href="">
+    //       <section className="card">
+    //         <h3>🛸 - Example 2 </h3>
+    //       </section>
+    //     </a>
+    //     <a id="" className="" href="">
+    //       <section className="card">
+    //         <h3>💀 - Example 3  </h3>
+    //       </section>
+    //     </a>
+    //     <a id="" className="" href="">
+    //       <section className="card">
+    //         <h3>🥑 - Example 4 </h3>
+    //       </section>
+    //     </a>
+    //     <a id="" className="" href="">
+    //       <section className="card">
+    //         <h3>👾 - Example 5 </h3>
+    //       </section>
+    //     </a>
+    //     <a id="" className="" href="">
+    //       <section className="card">
+    //         <h3>🌌 - Example 6 </h3>
+    //       </section>
+    //     </a>
+    //   </section>
+    // </section>
+  )
 }
