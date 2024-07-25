ff8197c0c4dcd18101432c4de01022f9f60f1610
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-07-25 11:03:13 -0400

    Footer complete

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,17 +4,17 @@ export default function Contact() {
         <h2>Contact Me</h2>
         <form id="form" className="">
             <br/>
-            <label for="contact-name">Name</label>
+            <label htmlFor="contact-name">Name</label>
             <section id="contact-name" className="form-group"> 
                 <input type="text" id="cName" className="" placeholder="Enter Your Name"></input>
             </section>
             <br/>
-            <label for="contact-email">Email</label>
+            <label htmlFor="contact-email">Email</label>
             <section id="contact-email" className="form-group">
                 <input type="text" id="cEmail" className="" placeholder="name@example.com"></input>
             </section>
             <br/>
-            <label for="contact-message">Message</label>
+            <label htmlFor="contact-message">Message</label>
             <section id="contact-message" className="form-group">
                 <textarea id="cMessage" className="form-control" aria-label="Message" rows="3"></textarea>
             </section>
